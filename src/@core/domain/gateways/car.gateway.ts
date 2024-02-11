29635b9c09d16055cb2b5d6f293af8d94d8255c9
yraffic02 <yraffic02@gmail.com>
2024-02-11 20:57:58 -0300

    feat: cirado  gateway para car

---
+++ src/@core/domain/gateways/car.gateway.ts
@@ -0,0 +1,10 @@
+import { Car, CarProps } from "../entities/car";
+
+
+export interface CarGateway {
+    findAll(): Promise<Car[]>; 
+    findById(id: string): Promise<Car>; 
+    add(car: CarProps): Promise<Car>;
+    update(id: string, car: CarProps): Promise<Car>; 
+    delete(id: string): Promise<boolean>;
+}
