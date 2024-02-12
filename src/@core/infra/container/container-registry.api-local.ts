729a2117d4f26c7972f6ead0b0e64c9e85f93198
yraffic02 <yraffic02@gmail.com>
2024-02-12 19:21:05 -0300

    chore: registry container para getCar

--- src/@core/infra/container/container-registry.api-local.ts
+++ src/@core/infra/container/container-registry.api-local.ts
@@ -2,12 +2,14 @@ import { Container } from "inversify";
 import { apiLocal } from "../api-local";
 import { CarHttpGateway } from "../gateways/cars-http.gateway";
 import { ListCarsUseCase } from "@/@core/application/car/list-cars-use-case";
+import { GetCarUseCase } from "@/@core/application/car/get-car.use-case";
 
 export const Registry = {
     AxiosAdapter: Symbol.for("AxiosAdapter"),
 
     CarGateway: Symbol.for("CarGateway"),
     ListCarUseCase: Symbol.for("ListCarUseCase"),
+    GetCarUseCase: Symbol.for("GetCarUseCase")
 }
 
 export const container = new Container;
@@ -20,4 +22,8 @@ container.bind(Registry.CarGateway).toDynamicValue((context)=>{
 
 container.bind(Registry.ListCarUseCase).toDynamicValue((context)=>{
     return new ListCarsUseCase(context.container.get(Registry.CarGateway))
+})
+
+container.bind(Registry.GetCarUseCase).toDynamicValue((context)=>{
+    return new GetCarUseCase(context.container.get(Registry.CarGateway))
 })
