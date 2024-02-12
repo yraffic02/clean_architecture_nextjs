58d2f9063a3150221b1111858073d0bcd46c331d
yraffic02 <yraffic02@gmail.com>
2024-02-11 21:54:40 -0300

    fix: corrigido erro type erro no schema

--- src/app/novo-carro/components/Form/index.tsx
+++ src/app/novo-carro/components/Form/index.tsx
@@ -2,28 +2,51 @@
 
 import { Input } from "@/components/Input"
 import { Label } from "@/components/Label"
+import { UseHookFormCar } from "@/utils/hooks/useFormCar"
 
 export const FormCars = () =>{
+    const {errors, handleSubmit, onSubmit, register} = UseHookFormCar()
     return (
-        <form className="flex flex-col justify-start w-2/6 p-7 gap-4">
+        <form 
+            className="flex flex-col justify-start w-2/6 p-7 gap-4"
+            onSubmit={handleSubmit(onSubmit)}
+        >
                 <Label labelHtmlFor="brand">
                     Marca
                 </Label>
                 <Input
                     id="brand" 
+                    {...register('brand')}
                 />
+                {errors.brand && 
+                    <span className="text-red-600">
+                        {errors.brand?.message}
+                    </span>
+                }
                 <Label labelHtmlFor="model">
                     Modelo
                 </Label>
                 <Input 
                     id="model"
+                    {...register('model')}
                 />
+                {errors.brand && 
+                    <span className="text-red-600">
+                        {errors.model?.message}
+                    </span>                    
+                }
                 <Label labelHtmlFor="year">
                     Ano
                 </Label>
                 <Input 
                     id="year"
+                    {...register('year')}
                 />
+                {errors.year && 
+                    <span className="text-red-600">
+                        {errors.year?.message}
+                    </span>
+                }
                 <button type="submit">Cadastrar</button>
         </form>
     )
--- src/utils/schema/carSchema.ts
+++ src/utils/schema/carSchema.ts
@@ -1,7 +1,7 @@
 import { CarProps } from '@/@core/domain/entities/car';
 import * as yup from 'yup';
 
-type CarFormType = CarProps
+export type CarFormType = CarProps
 
 export const CarScheme: yup.ObjectSchema<CarFormType> = yup.object().shape({
     brand: yup.string().required('A marca do carro é obrigatória.'),
@@ -9,8 +9,8 @@ export const CarScheme: yup.ObjectSchema<CarFormType> = yup.object().shape({
     year: yup
         .number()
         .required('O ano do carro é obrigatório.')
+        .typeError('O ano do carro deve ser um número.')
         .integer('O ano do carro deve ser um número inteiro.')
-        .min(1900, 'O ano do carro deve ser igual ou maior que 1900.')
         .max(new Date().getFullYear(), 'O ano do carro não pode ser maior que o ano atual.')
         .min(new Date().getFullYear() - 15, 'O carro não pode ter mais de 15 anos.')
 })
