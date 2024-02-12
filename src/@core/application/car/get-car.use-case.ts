09a7607cdf05903f7a0d182da1d2406a72aaa4bf
yraffic02 <yraffic02@gmail.com>
2024-02-12 19:19:25 -0300

    feat:  criado use case para pegar apenas um car

--- src/@core/application/car/get-car.use-case.ts
+++ src/@core/application/car/get-car.use-case.ts
@@ -1,11 +1,10 @@
 import { Car } from "@/@core/domain/entities/car";
 import { CarGateway } from "@/@core/domain/gateways/car.gateway";
 
-
-export class ListCarsUseCase {
+export class GetCarUseCase {
     constructor(private CarGate: CarGateway){}
 
-    async execute(): Promise<Car[]>{
-        return this.CarGate.findAll();
+    async execute(id: string): Promise<Car>{
+        return this.CarGate.findById(id)
     }
 }
