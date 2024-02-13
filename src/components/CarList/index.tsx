865bfa8fd76402bbaba832acacc716cfd6e42056
yraffic02 <yraffic02@gmail.com>
2024-02-13 16:00:51 -0300

    style: ajuste feito no estilo do car list

--- src/components/CarList/index.tsx
+++ src/components/CarList/index.tsx
@@ -10,33 +10,43 @@ export const CarList = ({cars}: CardListProps) =>{
   const {push} = useRouter()
   
     return (
-        <div className="flex flex-col items-start gap-2 border-red-700 border-2 rounded-md p-3 w-2/5  h-96">
-        <div className="flex items-start gap-6 w-full">
-          <h2>Marca</h2>
-          <h2>Modelo</h2>
-          <h2>Ano</h2>
-        </div>
-        <ul>
-          {
-            cars.length <= 0 && <h1>Não há carros registrados!!</h1>
-          }
-          {
-            cars.length > 0 && cars.map((item, index)=>{
-              return (
-                <div className="flex items-start gap-6 w-full">
+        <div className="border-red-700 border-2 w-2/5  h-96 flex flex-col items-start gap-2 rounded-md ">
+          <div className="flex items-start gap-6 bg-slate-500 text-white w-full p-4">
+            <div className="flex items-center justify-between w-2/4">
+              <h2>Marca</h2>
+              <h2>Modelo</h2>
+              <h2>Ano</h2>
+            </div>
+          </div>
+          <div className="overflow-auto w-full">
+          <ul className="p-4">
+            {
+              cars.length <= 0 && <h1>Não há carros registrados!!</h1>
+            }
+            {
+              cars.length > 0 && cars.map((item, index)=>{
+                return (
                   <li 
                     key={index} 
                     onClick={()=> push(`/carro/${item.carProps.id}`)}
                   >
-                    {item.carProps.brand} | {item.carProps.model}
+                    <div className="flex justify-between gap-6 w-full">
+                      <div className="flex items-center justify-between  w-2/4">
+                        <p>{item.carProps.brand}</p>
+                        <p>{item.carProps.model}</p> 
+                        <p>{item.carProps.year}</p>
+                      </div>
+                      <div className="flex items-center gap-4">
+                        <button>editar</button>
+                        <button>apagar</button>
+                      </div>
+                    </div>
                   </li>
-                  <button>editar</button>
-                  <button>apagar</button>
-                </div>
-              )
-            })
-          }
-        </ul>
+                )
+              })
+            }
+          </ul>
+        </div>
       </div>
     )
 }
