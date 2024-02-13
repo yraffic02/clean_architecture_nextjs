6de5186bde72f8f27f4dc98c636c1acfa12ecc0d
yraffic02 <yraffic02@gmail.com>
2024-02-12 22:02:43 -0300

    chore: fazendo detalhamento de carro

--- src/app/carro/[id]/page.tsx
+++ src/app/carro/[id]/page.tsx
@@ -1,8 +1,24 @@
-export default function CarDetailPage(){
-    
+import { GetCarUseCase } from "@/@core/application/car/get-car.use-case"
+import { Registry, container } from "@/@core/infra/container/container-registry.api-local"
+import { redirect } from "next/navigation"
+
+async function getCar(id:string) {
+    const useCaseGetCar = container.get<GetCarUseCase>(Registry.GetCarUseCase)
+    const car = await useCaseGetCar.execute(id)
+
+    return car
+}
+
+export default async function CarDetailPage({ params }: { params: { id: string } }){
+    const car = await getCar(params.id)    
+    console.log(car)
+
+
     return(
-        <div>
-            oi
+        <div className="p-6 flex flex-col gap-6">
+            <h1>{car.carProps.brand}</h1>
+            <h2>{car.carProps.model}</h2>
+            <p>{car.carProps.year}</p>
         </div>
     )
 }
