3802f011ecf833ea8f71af2de3692bc6580ec167
yraffic02 <yraffic02@gmail.com>
2024-02-14 17:18:12 -0300

    style: mudanças nas cores da logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default async function Home (){
   return (
-    <main className="flex flex-col items-center justify-center p-5">
+    <main className="flex flex-col items-center justify-center p-5 gap-4">
       <h1>Carros Cadastrados no sistema</h1>
       <CarList /> 
       <Link href='/novo-carro' prefetch={true} >Adicionar Carro</Link>
