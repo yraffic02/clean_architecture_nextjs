f989307f9e6c1e46f2e0799d6efbd67d0ca0520b
yraffic02 <yraffic02@gmail.com>
2024-02-14 18:15:45 -0300

    style: ajustando style page home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,14 @@ import Link from "next/link";
 export default async function Home (){
   return (
     <main className="flex flex-col items-center justify-center p-5 gap-4">
-      <h1>Carros Cadastrados no sistema</h1>
+      <h1 className="font-bold text-xl">Carros Cadastrados no sistema</h1>
       <CarList /> 
-      <Link href='/novo-carro' prefetch={true} >Adicionar Carro</Link>
+      <Link 
+        href='/novo-carro' 
+        className="font-bold"
+      >
+        Adicionar Carro
+      </Link>
     </main>
   );
 }
