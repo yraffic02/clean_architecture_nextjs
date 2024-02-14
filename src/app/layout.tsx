2934d3136421c882008233466e9eeb831c07d1fd
yraffic02 <yraffic02@gmail.com>
2024-02-14 17:12:23 -0300

    feat: criado header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { Header } from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ToastContainer />
+        <Header/>
         {children}
       </body>
     </html>
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image"
+
+export const Header = () =>{
+    return (
+        <header className="flex items-center p-6 bg-blue-950">
+            <div className="flex items-center gap-5 bg-slate-500 p-2 rounded-md">
+                <Image 
+                    src='/carlogo.svg'
+                    height={30}
+                    width={30}
+                    alt="car logo"
+                />
+                <h1 className="text-white font-bold">Carros</h1>
+            </div>
+        </header>
+    )
+}
