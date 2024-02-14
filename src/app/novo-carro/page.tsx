7ac04edabf62a07c4129fe69c8951dd15ae9a0c9
yraffic02 <yraffic02@gmail.com>
2024-02-14 18:23:52 -0300

    style: title page novo carro ajustado

--- src/app/novo-carro/page.tsx
+++ src/app/novo-carro/page.tsx
@@ -3,7 +3,7 @@ import { FormCars } from "./components/Form";
 export default function NewCar(){
     return(
         <div className="flex flex-col justify-center items-center p-4">
-            <h1>novo carro</h1>
+            <h1 className="font-bold text-lg">novo carro</h1>
             <FormCars />
         </div>
     )
