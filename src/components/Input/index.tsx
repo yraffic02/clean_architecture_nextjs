bbf55ddfdaa6f997f5839378d5c40d99302b92b6
yraffic02 <yraffic02@gmail.com>
2024-02-14 18:20:00 -0300

    style: bordas adicionada ao componente input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,7 +4,12 @@ type InputProps = InputHTMLAttributes<HTMLInputElement>;
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(({name, ...rest}, ref)=>{
     return (
-        <input {...rest} name={name} ref={ref}/>
+        <input 
+            {...rest}  
+            name={name} 
+            ref={ref}
+            className="border-gray-700 border-2 rounded-lg p-1"
+        />
     )
 })
 
